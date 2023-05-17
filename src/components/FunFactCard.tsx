f5296cb3ecb6950d7ef33cf337f03046504bfe83
Duc Phan <ptmdmusic@gmail.com>
2023-05-16 22:19:47 -0700

    feat: add data for FunFactCard and remove unused css

--- src/components/FunFactCard.tsx
+++ src/components/FunFactCard.tsx
@@ -3,14 +3,13 @@
 import { funFactList } from "@/data/fun-fact";
 import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
-import style from "./FunFactCard.module.css";
 
 export const FunFactCard = () => {
   const [factIndex, setFactIndex] = useState(getRandomIndex());
 
   return (
-    <Card className={style["fun-fact-card"]}>
-      <Card.Body className={style["card-body"]}>
+    <Card>
+      <Card.Body>
         <Card.Title>Fun Fact!</Card.Title>
 
         <Card.Text>{funFactList[factIndex]}</Card.Text>
@@ -18,7 +17,6 @@ export const FunFactCard = () => {
         <Button
           variant="outline-primary"
           onClick={() => setFactIndex(getRandomIndex())}
-          className={style["card-cta"]}
         >
           Next Fact!
         </Button>
---
+++ src/data/fun-fact.ts
@@ -0,0 +1,10 @@
+// https://fungenerators.com/random/facts/movies/pokeacutemon
+export const funFactList = [
+  "In 1997 a Pokémon episode called Dennō Senshi Porygon aired that resulted in 635 Japanese children having epileptic seizures. These children suffered the seizures because of the strobe light effects in the episode. The episode was never aired again.",
+  "Saudi Arabia banned Pokémon in 2001 because of some symbols used in the trading cards and games and because it includes gambling which violates Muslim faith doctrine.",
+  "The first Pokémon movie was released in 1998 called Pokémon: The First Movie-Mewtwo Strikes Back. By 2015 18 Pokémon movies had been produced.",
+  "The most valuable Pokémon cards are the Pikachu Illustrator Cards, worth as much as $100,000. Only a few of the cards exist.",
+  "The Pokémon Company generates an average of $1.5 billion each year. This places the Pokémon Company ahead of the Coca-Cola Company, Sony Pictures, and the NHL in terms of its profitability as a license. Pokémon is just behind The Hershey Company in profitability.",
+  "In Japanese folklore the tapir is capable of eating dreams and nightmares. The Pokémon character Drowzee is based upon the tapir.",
+  // TODO: add more fun facts
+] as const;
