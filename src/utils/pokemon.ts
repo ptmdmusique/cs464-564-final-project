1769cb74eb6ed622a9918032963291fbf6c35707
Duc Phan <ptmdmusic@gmail.com>
2023-05-28 15:40:29 -0700

    feat: optimize pokemon calls via cache and add move client

--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,9 +1,18 @@
-import { PokemonClient } from "pokenode-ts";
+import { MoveClient, PokemonClient } from "pokenode-ts";
+import { fromCache } from "./cache.";
 import { getRandomNumber } from "./functional";
 
 const MIN_POKEMON_ID = 1;
 const MAX_POKEMON_ID = 1000;
 
-const api = new PokemonClient();
+const pokemonClient = new PokemonClient();
+const moveClient = new MoveClient();
+
+export const getPokemonById = (id: number) =>
+  fromCache("pokemon", () => pokemonClient.getPokemonById(id), id);
+
 export const getRandomPokemon = () =>
-  api.getPokemonById(getRandomNumber(MIN_POKEMON_ID, MAX_POKEMON_ID));
+  getPokemonById(getRandomNumber(MIN_POKEMON_ID, MAX_POKEMON_ID));
+
+export const getMoveById = (id: number) =>
+  fromCache("move", () => moveClient.getMoveById(id), id);
