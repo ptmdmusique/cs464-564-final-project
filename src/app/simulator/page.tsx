922bdc00c3d7ed64091c531bf7d474aee0d46788
Duc Phan <ptmdmusic@gmail.com>
2023-05-29 15:33:31 -0700

    fix: tweak more type tag color

--- src/app/simulator/page.tsx
+++ src/app/simulator/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { BattleResultCard } from "@/components/BattleResultCard";
 import { PageLayout } from "@/components/PageLayout";
 import { BattleResult } from "@/data/battle";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Row } from "react-bootstrap";
 const PokemonBattleView = dynamic(() => import("@/views/PokemonBattleView"), {
   ssr: false,
 });
@@ -11,6 +13,13 @@ const PokemonBattleView = dynamic(() => import("@/views/PokemonBattleView"), {
 const SimulatorPage = () => {
   const [battleResult, setBattleResult] = useState<BattleResult | null>(null);
   const [isBattling, setIsBattling] = useState(false);
+
+  const resultRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (isBattling) {
+      resultRef.current?.scrollIntoView({ behavior: "smooth", block: "end" });
+    }
+  }, [isBattling]);
 
   return (
     <PageLayout className="pb-4">
@@ -20,6 +29,10 @@ const SimulatorPage = () => {
         onBattleResultChange={setBattleResult}
         onIsBattlingChange={setIsBattling}
       />
+
+      <Row className="mt-4 gx-0" ref={resultRef}>
+        <BattleResultCard battleResult={battleResult} isBattling={isBattling} />
+      </Row>
     </PageLayout>
   );
 };
